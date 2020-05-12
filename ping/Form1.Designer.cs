0cdbfa83a0e7b080158624b6a0b757b4dfe3b191
angelop <angelop@ua.pt>
2020-05-12 20:50:06 +0100

    Add project files.

---
+++ ping/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer_ping = new System.Windows.Forms.Timer(this.components);
+            this.btn_start = new System.Windows.Forms.Button();
+            this.ip_textbox = new System.Windows.Forms.TextBox();
+            this.textout = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // timer_ping
+            // 
+            this.timer_ping.Interval = 1000;
+            this.timer_ping.Tick += new System.EventHandler(this.timer_ping_Tick);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(73, 55);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // ip_textbox
+            // 
+            this.ip_textbox.Location = new System.Drawing.Point(239, 55);
+            this.ip_textbox.Name = "ip_textbox";
+            this.ip_textbox.Size = new System.Drawing.Size(100, 20);
+            this.ip_textbox.TabIndex = 1;
+            // 
+            // textout
+            // 
+            this.textout.Location = new System.Drawing.Point(396, 55);
+            this.textout.Name = "textout";
+            this.textout.Size = new System.Drawing.Size(328, 205);
+            this.textout.TabIndex = 2;
+            this.textout.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textout);
+            this.Controls.Add(this.ip_textbox);
+            this.Controls.Add(this.btn_start);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer_ping;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.TextBox ip_textbox;
+        private System.Windows.Forms.RichTextBox textout;
+    }
+}
+
---
+++ ping/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+//usadas no ping
+using System.Net;
+using System.Net.NetworkInformation;
+///
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// olols
+        /// </summary>
+        /// <param name="sender">frewdf</param>
+        /// <param name="e">fee4fgftg45</param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer_ping.Enabled = false;
+            timer_ping.Interval = 5000;
+            ip_textbox.Text = "192.168.1.66";
+        }
+
+
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            if (timer_ping.Enabled == false)
+            {
+                timer_ping.Enabled = true;
+                btn_start.Text = "Stop";
+                ip_textbox.Enabled = false;
+            }
+            else
+            {
+                timer_ping.Enabled = false;
+                btn_start.Text = "Start";
+                ip_textbox.Enabled = true;
+            }
+
+        }
+
+        private void timer_ping_Tick(object sender, EventArgs e)
+        {
+            sendping(ip_textbox.Text);
+        }
+
+      
+            // args[0] can be an IPaddress or host name.
+        public void sendping(string args)
+        {
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+
+            // Use the default Ttl value which is 128,
+            // but change the fragmentation behavior.
+            options.DontFragment = true;
+
+            // Create a buffer of 32 bytes of data to be transmitted.
+            string data = "aaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+            try
+            {
+                PingReply reply = pingSender.Send(args, timeout, buffer, options);
+                if (reply.Status == IPStatus.Success)
+                {
+                    Console.WriteLine("Address: {0}", reply.Address.ToString());
+                    Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
+                    Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+                    Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
+                    Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+
+                    textout.AppendText("Address: {0}"+ reply.Address.ToString()+"\n");
+                    textout.AppendText("RoundTrip time: {0}"+ reply.RoundtripTime + "\n");
+                    textout.AppendText("Time to live: {0}"+ reply.Options.Ttl + "\n");
+                    textout.AppendText("Don't fragment: {0}"+ reply.Options.DontFragment + "\n");
+                    textout.AppendText("Buffer size: {0}"+ reply.Buffer.Length + "\n");
+
+                    
+
+                }
+                else
+                {
+                    Console.WriteLine("host not reached");
+                    textout.AppendText("host not reached\n");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error: " );
+                timer_ping.Enabled = false;
+                
+            }
+
+        }
+       
+    }
+}
